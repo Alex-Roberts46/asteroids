cc9e53cdb8201bc54b7f67dda6343849455e7fc6
Alex_Roberts46 <noinsong46@gmail.com>
2025-05-09 19:57:18 -0500

    added updateable and drawable groups

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -16,8 +19,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
